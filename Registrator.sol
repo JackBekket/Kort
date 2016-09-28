8f9d662b787e8e836bea5231dee5ec0d7fb4944f
JackBekket <jackatwork@yandex.ru>
2016-09-28 03:49:52 -0700

    Multiple Notarisus

--- Registrator.sol
+++ Registrator.sol
@@ -4,6 +4,8 @@
 #  This contracts allow you to automatically register contract you create.
 #  And probably (to the moment of reading this will be implemented) signed it (verify)
 #
+#   This version worked without DAO inplementation
+#   This version suuport multiply Notariuses
 #
 */
 contract owned {
@@ -27,29 +29,34 @@ contract Registrator  is owned{
 
 
 
-string public standard = 'registrator 0.0.1';
+string public standard = 'registrator 0.0.2';
 string public version = '0.0.1';
-string public type = 'noncommercial';
+string public type = 'noncommercial/nonDAO';
 
+//address public notarius = owner;
 //uint public price;
 
 //  mapping (address => mapping (uint => string)) public Links;
 // mapping (address => uint256) public balanceOf;
 
 // Array of Owner - Contract - Signed by???
-mapping (address => address) public Links;
+mapping (address => mapping(string => address)) public Links;
 mapping (address => mapping (address => address)) public Certs;
+mapping (address => bool) public Notariuses;
+
+
 event Registred (address client, address _contract);
 event Signed(address client, address _contract, address signedby);
 
 //initialization
-function Registrator (uint pricePlace){
+function Registrator (){
 //price = pricePlace;
-
+// address public notarius = owner;
+Notariuses[msg.sender] = true;
 }
 
 // Probably need to use msg.sender instead of address _contract. Test this feauture later
-function register (address _owner,address _contract){
+function register (address _owner,string contractname,address _contract){
 /*
 if (amount<price) throw;
 balance0f[msg.sender]=amount;
@@ -59,19 +66,24 @@ Registred(client,amount,link);
 */
 
 
-Links[_owner]=_contract;
-Registred(_owner,_contract);
+Links[_owner][contractname]=_contract;
+Registred(_owner,contractname,_contract);
 }
 
 
-function sign (address _owner,address _contract, address notarius) onlyOwner {
+function sign (address _owner,address _contract)  {
+// address notarius = msg.sender;
+if (Notariuses[msg.sender]!=true) throw;
+address notarius = msg.sender;
+
 Certs[_owner][_contract]=notarius;
  //uint date = now * 1 minutes;
 Signed(_owner,_contract,_notarius);
 
-
-
 }
+
+
+
 
 
 /*
